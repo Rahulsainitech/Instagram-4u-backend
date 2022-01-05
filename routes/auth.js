d23ff6956fb014db20870cb9fef0716f6b5f1e27
Rahulsainitech <rahuliim2001@gmail.com>
2022-01-05 11:54:31 +0530

    correct file path

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const {secretKey} = require('../keys')
+const {secretKey} = require('..config/keys')
 const middleware = require('../middleware/requireLogin')
 
 router.get('/protected',middleware, (req, res) => {
